2735d856f96129d54faff0f4c370c3839a897832
Damain Ramsajan <dpramsajan@gmail.com>
2020-12-19 07:51:32 +0100

    Sidebar Header Complete

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import "./Sidebar.css"
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord"
+import CreateIcon from "@material-ui/icons/Create"
 
 function Sidebar() {
     return (
-        <div>
+        <div className = "sidebar">
+            <div className = "sidebar__header">
+                <div className = "sidebar__info">
+                    <h2>Intelica</h2>
+                    <h3><FiberManualRecordIcon/>Damain Ramsajan </h3>
+                </div>
+                <CreateIcon/>
+            </div>
             <h1>Sidebar</h1>
         </div>
     )
